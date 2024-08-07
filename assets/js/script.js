b88d7712fec972f8132e4d66845708c4d529f20d
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-08-07 16:15:28 -0600

    beginning code

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+function getCurrentWeather() {
+    // fetch request gets a list of all the repos for the node.js organization
+    const requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';}
